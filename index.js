50ed2b882f3781607d6231bb34216d6b56d89a23
Tanner Norris <jiujitsucarrot@Tanners-MacBook-Air.local>
2023-08-22 22:05:41 -0700

    Added questions, set class constructors.

--- index.js
+++ index.js
@@ -1,7 +1,40 @@
 const inquirer = require('inquirer');
+const generateLogo = require('./lib/generateLogo.js');
 
-function init() {
+function main () {
+    inquirer.prompt([
+        {
+            name: 'text',
+            message: 'Enter up to three characters:'
+        },
 
+        {
+            name: 'textColor',
+            message: 'Enter text color (keyword or hexadecimal):'
+        },
+
+        {
+            name: 'shape',
+            type: 'list',
+            message: 'Choose a shape:',
+            choices: ['triangle', 'circle', 'square']
+        },
+
+        {
+            name: 'shapeColor',
+            message: 'Enter shape color (keyword or hexadecimal):'
+        }
+    ])
+
+    .then(answers => {
+        generateLogo(answers.text, answers.textColor, answers.shape, answers.shapeColor);
+
+        console.log("Generated logo.svg");
+    })
+
+    .catch(error => {
+        console.error("An error occurred:", error);
+    });
 }
 
-init();
+main();
